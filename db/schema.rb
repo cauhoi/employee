2b3837db1a40a6aa05b235742cd5d2ae4e74ddba
Damien Le <damien.le@gocanvas.com>
2017-06-14 12:19:07 -0400

    update database migration file

--- db/schema.rb
+++ db/schema.rb
@@ -13,21 +13,21 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "departments", primary_key: "dept_no", force: true do |t|
+  create_table "departments", force: true do |t|
     t.string "dept_name", limit: 40, null: false
   end
 
   add_index "departments", ["dept_name"], name: "dept_name", unique: true, using: :btree
 
-  create_table "dept_emp", id: false, force: true do |t|
-    t.integer "emp_no",              null: false
-    t.string  "dept_no",   limit: 4, null: false
-    t.date    "from_date",           null: false
-    t.date    "to_date",             null: false
+  create_table "dept_emps", id: false, force: true do |t|
+    t.integer "employee_id",                          null: false
+    t.string  "department_id", limit: 4, default: "", null: false
+    t.date    "from_date",                            null: false
+    t.date    "to_date",                              null: false
   end
 
-  add_index "dept_emp", ["dept_no"], name: "dept_no", using: :btree
-  add_index "dept_emp", ["emp_no"], name: "emp_no", using: :btree
+  add_index "dept_emps", ["department_id"], name: "dept_no", using: :btree
+  add_index "dept_emps", ["employee_id"], name: "emp_no", using: :btree
 
   create_table "dept_manager", id: false, force: true do |t|
     t.string  "dept_no",   limit: 4, null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "dept_manager", ["dept_no"], name: "dept_no", using: :btree
   add_index "dept_manager", ["emp_no"], name: "emp_no", using: :btree
 
-  create_table "employees", primary_key: "emp_no", force: true do |t|
+  create_table "employees", force: true do |t|
     t.date   "birth_date",            null: false
     t.string "first_name", limit: 14, null: false
     t.string "last_name",  limit: 16, null: false
@@ -48,21 +48,21 @@ ActiveRecord::Schema.define(version: 0) do
   end
 
   create_table "salaries", id: false, force: true do |t|
-    t.integer "emp_no",    null: false
-    t.integer "salary",    null: false
-    t.date    "from_date", null: false
-    t.date    "to_date",   null: false
+    t.integer "employee_id", null: false
+    t.integer "salary",      null: false
+    t.date    "from_date",   null: false
+    t.date    "to_date",     null: false
   end
 
-  add_index "salaries", ["emp_no"], name: "emp_no", using: :btree
+  add_index "salaries", ["employee_id"], name: "emp_no", using: :btree
 
   create_table "titles", id: false, force: true do |t|
-    t.integer "emp_no",               null: false
-    t.string  "title",     limit: 50, null: false
-    t.date    "from_date",            null: false
+    t.integer "employee_id",            null: false
+    t.string  "title",       limit: 50, null: false
+    t.date    "from_date",              null: false
     t.date    "to_date"
   end
 
-  add_index "titles", ["emp_no"], name: "emp_no", using: :btree
+  add_index "titles", ["employee_id"], name: "emp_no", using: :btree
 
 end
